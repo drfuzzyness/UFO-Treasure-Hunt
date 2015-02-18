fe45a3fe56961da7044358d346b3127bf156a9e2
Matthew Conto <mbsconto@gmail.com>
2015-02-17 21:14:31 -0500

    Flasher, Fonts, and Terrain

---
+++ Assets/Scripts/Flasher.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Flasher : MonoBehaviour {
+
+	public float flashDuration;
+	public int numFlashes;
+
+	private float flashTimer = 0f;
+	private int numFlashesTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+		reset();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		flashTimer += Time.deltaTime;
+		if( flashTimer >= flashDuration ) {
+			flashTimer = 0;
+			toggleVisible();
+			numFlashesTimer++;
+			if( numFlashesTimer >= numFlashes ) {
+				gameObject.SetActive( false );
+			}
+		}
+	}
+
+	void toggleVisible() {
+		if( gameObject.GetComponent<Text>().enabled ){
+			gameObject.GetComponent<Text>().enabled = false;
+		}
+		else {
+			gameObject.GetComponent<Text>().enabled = true;
+		}
+	}
+
+	void reset() {
+		gameObject.GetComponent<Text>().enabled = true;
+		flashTimer = 0f;
+		numFlashesTimer = 0;
+	}
+}
---
+++ Assets/Scripts/UFOSpinner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UFOSpinner : MonoBehaviour {
+
+	public float passiveRotationSpeed;
+
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate( new Vector3( 0f, passiveRotationSpeed * Time.deltaTime, 0f) );
+
+	}
+}
